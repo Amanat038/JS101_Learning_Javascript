3b097d483b89fc5760d44122f3bd4e69c6cdbca1
unknown <amanatsingh872001@gmail.com>
2023-02-05 10:33:51 +0530

    add third day commit

---
+++ LogicalOperatersTernaryOperater/problem3.js
@@ -0,0 +1,10 @@
+
+
+let character = "d";
+
+if((character != "a")&&(character !="e")&&(character !="i")&&(character !="o")&&(character !="u")){
+  console.log("consonant");
+}
+else{
+  console.log("vowel");
+}
---
+++ LogicalOperatersTernaryOperater/problem4.js
@@ -0,0 +1,14 @@
+
+let a = 900;
+let b = 100;
+let c = 600;
+
+if((a > b)&&(a>c)){
+  console.log("a is greater");
+}
+  else if((b > a)&&(b > c)){
+    console.log("b is greater");
+}
+  else if((c > a)&&(c > b)){
+    console.log("c is greater");
+}
